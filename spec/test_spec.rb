18f5c3500eac9bd366091719a761a9df231cd777
blackstarss <kota.weeds1204@gmail.com>
2021-12-12 04:32:11 +0000

    [start]rspec_app

---
+++ spec/test_spec.rb
@@ -0,0 +1,12 @@
+describe '四則演算' do
+  context '足し算' do
+    it '1 + 1 は 2 になる' do
+      expect(1 + 1).to eq 2
+    end
+  end
+  context '足し算' do
+    it '2 + 2 は 4 になる' do
+      expect(2 + 2).to eq 3
+    end
+  end
+end
